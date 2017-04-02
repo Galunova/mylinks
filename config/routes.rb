2d6d099e4e8b6db7c45d016860f61ec11bdc5d69
Adelina Galunova <adelina.vitamine@gmail.com>
2017-04-02 19:40:00 +0600

    Add show user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'links#index'
   devise_for :users
+  resources :users, only: :show
   resources :links, except: :index
 
   get "*path", to: "application#render_not_found"
