c300200250e34a66b4398a24e57af919925ce7e2
Adelina Galunova <adelina.vitamine@gmail.com>
2017-04-02 21:01:26 +0600

    Create and update link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,24 +1,34 @@
 class LinksController < ApplicationController
-  
+
   def index
-  end
-
-  def create
-  end
-
-  def new
-  end
-
-  def edit
   end
 
   def show
   end
 
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.create(links_params)
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
   def update
+    @link = Link.update(links_params)
   end
 
   def destroy
+  end
+
+private
+
+  def links_params
+    params.require(:link).permit(:url, :private)
   end
 
 end
